783a9cfa9af5b56e0c7fb6d59a4a38f6cc3498dd
Madhav Sharma <83121276+maddy1381@users.noreply.github.com>
2021-06-04 17:59:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// Init Github
+const github = new Github;
+// Init UI
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listener
+searchUser.addEventListener('keyup', (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if(userText !== ''){
+   // Make http call
+   github.getUser(userText)
+    .then(data => {
+      if(data.profile.message === 'Not Found') {
+        // Show alert
+
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+      }
+    })
+  } else {
+    // Clear profile
+    
+  }
+}); 
